df4852755bff8129163f52237ab83e28f82c44fe
dahxxn <dahyeonhan4529@gmail.com>
2025-11-01 23:01:34 +0900

    refactor(input): unify input reading flow

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,21 +5,19 @@ import lotto.view.message.InputMessage;
 
 public class InputView {
     public static String readPurchaseAmount() {
-        System.out.println(InputMessage.PURCHASE_AMOUNT);
-        return readRawInput();
+        return readRawInput(InputMessage.PURCHASE_AMOUNT);
     }
 
     public static String readWinningNumbers() {
-        System.out.println(InputMessage.WINNING_NUMBERS);
-        return readRawInput();
+        return readRawInput(InputMessage.WINNING_NUMBERS);
     }
 
     public static String readBonusNumber() {
-        System.out.println(InputMessage.BONUS_NUMBER);
-        return readRawInput();
+        return readRawInput(InputMessage.BONUS_NUMBER);
     }
 
-    private static String readRawInput() {
+    private static String readRawInput(String inputMessage) {
+        System.out.println(inputMessage);
         return Console.readLine();
     }
 }
