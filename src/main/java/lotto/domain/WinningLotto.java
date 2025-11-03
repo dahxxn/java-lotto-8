d6ff7e00132835e399a2d5232e97cc74ca171c36
dahxxn <dahyeonhan4529@gmail.com>
2025-11-03 13:57:07 +0900

    refactor(winning-lotto): simplify countMatches logic using Stream API

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,9 +3,7 @@ package lotto.domain;
 import static lotto.domain.error.ErrorMessage.WINNING_LOTTO_ERROR_NOT_NUMERIC;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class WinningLotto {
     private static final String SPLIT_REGEX = ",";
@@ -22,12 +20,9 @@ public class WinningLotto {
     }
 
     public int countMatches(Lotto lotto) {
-        Set<Integer> totalNumbers = new HashSet<>(lotto.numbers());
-        totalNumbers.addAll(winningNumbers.numbers());
-
-        int lottoNumberCount = lotto.numbers().size();
-        int winningNumberCount = winningNumbers.numbers().size();
-        return (lottoNumberCount + winningNumberCount) - totalNumbers.size();
+        return (int) lotto.numbers().stream()
+                .filter(winningNumbers.numbers()::contains)
+                .count();
     }
 
     private List<Integer> preprocessWinningNumbersInput(String winningNumbersInput) {
